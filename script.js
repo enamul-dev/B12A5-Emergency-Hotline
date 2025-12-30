66acd9a83e2674a84d6f6c8a750be7c226f12c21
enamul <enamul.dev01@gmail.com>
2025-12-30 13:33:35 +0600

    coin reduce functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// heart count
+
 let count = 0;
 let btn = document.getElementsByClassName("heart-click");
 let update = document.getElementById("nav-heart-count");
@@ -8,3 +10,23 @@ for (let i = 0; i < btn.length; i++) {
     count++;
   };
 }
+
+// Call & Coin functions
+let coins = 100;
+let coinDisplay = document.getElementById("coin-count");
+let callButtons = document.querySelectorAll(".call-btn");
+
+for (let i = 0; i < callButtons.length; i++) {
+  callButtons[i].onclick = function() {
+
+    if (coins >= 20) { 
+        coins -= 20; 
+        coinDisplay.textContent = coins; 
+        let serviceName = callButtons[i].getAttribute("data-name");
+        let serviceNumber = callButtons[i].getAttribute("data-number");
+        alert("Calling " + serviceName + " at " + serviceNumber);
+  } else{
+    alert("Not enough coins!"); 
+  };
+};
+};
